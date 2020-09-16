c6565aea994d2f00ec4286bf66ae44b574638805
Elvis Gastelum <43228550+ElvisGastelum@users.noreply.github.com>
2020-09-16 10:06:01 -0700

    Update ErrorBoundary.js

--- src/components/error-boundary/ErrorBoundary.js
+++ src/components/error-boundary/ErrorBoundary.js
@@ -8,7 +8,6 @@ import { ErrorMessage, ErrorMessageWrapper } from './styled';
  * @param {{children: React.ReactElement, error: {message: string, errorEvent: Error}}} props used for this component
  */
 export default function ErrorBoundary({ children, error }) {
-  console.log(error);
   if (error) {
     return (
       <>
